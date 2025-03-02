1bf71f8564fcfd1145b934d40780c55d9c2de844
Peter Kin <kinpeter85@gmail.com>
2025-03-02 08:59:40 +0100

    Rework api build script

--- api/scripts/build-publish.mjs
+++ api/scripts/build-publish.mjs
@@ -1,23 +1,49 @@
 import { readFileSync, writeFileSync } from 'fs';
 import { execSync } from 'child_process';
+import { checkDependencies } from './check-dependencies.mjs';
+import readline from 'readline';
 
-const packageJson = JSON.parse(readFileSync('./api.package.json', 'utf-8'));
-const versionParts = packageJson.version.split('.');
-versionParts[2] = (parseInt(versionParts[2], 10) + 1).toString();
-const newVersion = versionParts.join('.');
-packageJson.version = newVersion;
-writeFileSync('./api.package.json', JSON.stringify(packageJson, null, 2));
-execSync('npx prettier --write ./api.package.json', { stdio: 'inherit' });
-console.log(`Version bumped to ${newVersion}`);
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-execSync(
-  `docker build -f ./api/docker/api.Dockerfile -t kinp/frazewise:${newVersion} -t kinp/frazewise:latest .`,
-  {
-    stdio: 'inherit',
-  }
-);
+const hasDependenciesChanged = checkDependencies();
 
-execSync(`docker push kinp/frazewise:${newVersion}`, { stdio: 'inherit' });
-execSync(`docker push kinp/frazewise:latest`, { stdio: 'inherit' });
+if (hasDependenciesChanged) {
+  rl.question('Dependencies have changed. Do you want to continue? ([y]es/[n]o) ', answer => {
+    if (answer.toLowerCase() !== 'yes' && answer.toLowerCase() !== 'y') {
+      console.log('Process aborted by the user.');
+      rl.close();
+      process.exit(0);
+    } else {
+      rl.close();
+      buildAndPublish();
+    }
+  });
+} else {
+  buildAndPublish();
+}
 
-console.log(`Docker image tagged with version ${newVersion} and latest has been pushed.`);
+function buildAndPublish() {
+  const packageJson = JSON.parse(readFileSync('./api.package.json', 'utf-8'));
+  const versionParts = packageJson.version.split('.');
+  versionParts[2] = (parseInt(versionParts[2], 10) + 1).toString();
+  const newVersion = versionParts.join('.');
+  packageJson.version = newVersion;
+  writeFileSync('./api.package.json', JSON.stringify(packageJson, null, 2));
+  execSync('npx prettier --write ./api.package.json', { stdio: 'inherit' });
+  console.log(`Version bumped to ${newVersion}`);
+
+  execSync(
+    `docker build -f ./api/docker/api.Dockerfile -t kinp/frazewise:${newVersion} -t kinp/frazewise:latest .`,
+    {
+      stdio: 'inherit',
+    }
+  );
+
+  execSync(`docker push kinp/frazewise:${newVersion}`, { stdio: 'inherit' });
+  execSync(`docker push kinp/frazewise:latest`, { stdio: 'inherit' });
+
+  console.log(`Docker image tagged with version ${newVersion} and latest has been pushed.`);
+}
--- api/scripts/check-dependencies.mjs
+++ api/scripts/check-dependencies.mjs
@@ -1,25 +1,31 @@
 import { readFileSync } from 'fs';
 import { execSync } from 'child_process';
 
-const packageJsonPath = './package.json';
+export function checkDependencies() {
+  const packageJsonPath = './package.json';
 
-// Get the current and previous state of package.json
-const currentPackageJson = readFileSync(packageJsonPath, 'utf8');
-const previousPackageJson = execSync('git show HEAD:package.json').toString();
+  // Get the current and previous state of package.json
+  const currentPackageJson = readFileSync(packageJsonPath, 'utf8');
+  const previousPackageJson = execSync('git show HEAD:package.json').toString();
 
-// Parse the JSON data
-const currentDependencies = JSON.parse(currentPackageJson).dependencies || {};
-const currentDevDependencies = JSON.parse(currentPackageJson).devDependencies || {};
-const previousDependencies = JSON.parse(previousPackageJson).dependencies || {};
-const previousDevDependencies = JSON.parse(previousPackageJson).devDependencies || {};
+  // Parse the JSON data
+  const currentDependencies = JSON.parse(currentPackageJson).dependencies || {};
+  const currentDevDependencies = JSON.parse(currentPackageJson).devDependencies || {};
+  const previousDependencies = JSON.parse(previousPackageJson).dependencies || {};
+  const previousDevDependencies = JSON.parse(previousPackageJson).devDependencies || {};
 
-// Check for changes
-const dependenciesChanged =
-  JSON.stringify(currentDependencies) !== JSON.stringify(previousDependencies);
-const devDependenciesChanged =
-  JSON.stringify(currentDevDependencies) !== JSON.stringify(previousDevDependencies);
+  // Check for changes
+  const dependenciesChanged =
+    JSON.stringify(currentDependencies) !== JSON.stringify(previousDependencies);
+  const devDependenciesChanged =
+    JSON.stringify(currentDevDependencies) !== JSON.stringify(previousDevDependencies);
 
-if (dependenciesChanged || devDependenciesChanged) {
+  return dependenciesChanged || devDependenciesChanged;
+}
+
+const isChanged = checkDependencies();
+
+if (isChanged) {
   console.warn('--------------------------------------------------------------');
   console.warn('⚠️ Warning: Dependencies have changed in package.json!');
   console.warn('Make sure the dependencies are up to date in api.package.json!');
